65f3abb01d1c31a93a79e310d4a4e2db8b690c61
Kent <kentsaeteurn@gmail.com>
2019-02-15 10:31:14 -0800

    install passport and passport-local pkgs and configureboth for authenticating users by their email and password (i.e. allow users to sign in)

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -4,6 +4,7 @@ const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const flash = require("express-flash");
+const passportConfig = require("./passport-config");
 
 module.exports = {
   init(app, express) {
@@ -18,6 +19,12 @@ module.exports = {
       cookie: { maxAge: 1.21e+9 }
     }));
     app.use(flash());
+
+    passportConfig.init(app);
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
 
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
---
+++ src/config/passport-config.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../db/models").User;
+const authHelper = require("../auth/helpers");
+
+module.exports = {
+  init(app){
+    // We initialize Passport and tell it to use sessions to keep track of authenticated users.
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    /*We instruct Passport to use the local strategy. Passport looks for properties called username and password in the body of the request by default, so we pass an option called usernameField to specify what property to use instead.*/
+    passport.use(new LocalStrategy({
+      usernameField: "email"
+    }, (email, password, done) => {
+      User.findOne({
+        where: {email}
+      })
+      .then((user) => {
+        if (!user || !authHelper.comparePass(password, user.password)) {
+          return done(null, false, { message: "Invalid email or password" });
+        }
+
+        // If all went well, we return the authenticated user.
+        return done(null, user);
+      })
+    }));
+
+    // serializeUser takes the authenticated user's ID and stores it in the session.
+    passport.serializeUser((user, callback) => {
+      callback(null, user.id);
+    });
+
+    // deserializeUser takes the ID stored in the session and returns the user associated with it.
+    passport.deserializeUser((id, callback) => {
+      User.findById(id)
+      .then((user) => {
+        callback(null, user);
+      })
+      .catch((err) => {
+        callback(err, user);
+      })
+    })
+  }
+}
